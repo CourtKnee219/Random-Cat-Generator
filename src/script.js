e9adbcd4d10c758b02d2b42d284176ce66789ad0
CourtneyKerr19 <court.knee219@gmail.com>
2025-06-09 02:26:07 -0500

    Define core data objects

---
+++ src/script.js
@@ -0,0 +1,30 @@
+// Objects
+const Settings = {
+    imageLimit: 1,
+    size: "full",
+    theme: "light"
+};
+
+const CatImage = {
+    id: "",
+    url: "",
+    width: 0
+};
+
+const UserFavorite = {
+    imageId: "",
+    favoritedAt: '',
+    notes: ""
+};
+
+const Category = {
+    id: null,
+    name: "",
+    description: ""
+};
+
+const Tag = {
+    id: 0,
+    name: "",
+    type: ""
+};
